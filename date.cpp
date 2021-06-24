a401f1aadd27e62dca634a287c071cbeddfb75f5
sina-negarandeh <negarandeh.sina@gmail.com>
2021-06-24 15:00:21 +0430

    Add base date class

---
+++ date.cpp
@@ -0,0 +1,8 @@
+#include "date.hpp"
+
+Date::Date(int year, int month, int day)
+{
+    year_ = year;
+    month_ = month;
+    day_ = day;
+}
---
+++ date.hpp
@@ -0,0 +1,14 @@
+#ifndef DATE_HPP
+#define DATE_HPP
+
+class Date
+{
+private:
+    int year_;
+    int month_;
+    int day_;
+public:
+    Date(int year, int month, int day);
+};
+
+#endif // DATE_HPP
