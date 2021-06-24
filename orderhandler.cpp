e0ae626a8d7009756f37482f7d5a274e0e04dd13
sinku <sininoir@gmail.com>
2021-06-24 21:16:45 +0430

    Change class hierarchy

--- orderhandler.cpp
+++ orderhandler.cpp
@@ -1,11 +1,44 @@
 #include "orderhandler.hpp"
 
+using namespace std;
+
 OrderHandler::OrderHandler(/* args */)
 {
 }
 
-void OrderHandler::makeNewOrder(int userID, Date date)
+void OrderHandler::requestMovingService(std::string username, ServiceCatalog &catalog, TaskHandler &taskHandler)
 {
-    int orderID = orders_.size();
-    orders_.push_back(Order(orderID, date));
+    string options = catalog.getMovingOptions();
+    string userOptions = displayMovingOptions(options);
+    Date userDate = displayDates();
+    makeNewOrder(username, userOptions, userDate, taskHandler);
+}
+
+std::string OrderHandler::displayMovingOptions(std::string options)
+{
+    string userOptions;
+    cout<<"options are:"<<endl<<options<<endl;
+    cout<<"input your options: ";
+    getline(cin, userOptions);
+    return userOptions;
+}
+ 
+Date OrderHandler::displayDates()
+{
+    cout<<"All dates are available!"<<endl<<"input year month day: ";
+    int year, month, day;
+    cin>>year>>month>>day;
+    Date userDate(year, month, day);
+    return userDate;
+}
+
+void OrderHandler::makeNewOrder(std::string username, std::string options, Date date, TaskHandler &taskHandler)
+{
+    OrderLineService orderLineService;
+    orderLineService.addService(options, date, taskHandler);
+    Order order(id_counter_, date, orderLineService);
+    id_counter_ += 1;
+    orders_.push_back(order);
+    dao_.write_to_database(order);
+    payment_.startTransaction(order.getTotal());
 }
--- orderhandler.hpp
+++ orderhandler.hpp
@@ -2,16 +2,27 @@
 #define ORDER_HANDLER_HPP
 
 #include <vector>
-#include "order.hpp"
+#include <string>
 #include "date.hpp"
+
+#include "order.hpp"
+#include "servicecatalog.hpp"
+#include "payment.hpp"
+#include "dao.hpp"
 
 class OrderHandler
 {
 private:
     std::vector<Order> orders_;
+    int id_counter_=0;
+    Payment payment_;
+    DAO dao_;
 public:
-    OrderHandler(/* args */);
-    void makeNewOrder(int userID, Date date);
+    OrderHandler();
+    void requestMovingService(std::string username, ServiceCatalog &catalog, TaskHandler &taskHandler);
+    std::string displayMovingOptions(std::string options);
+    Date displayDates();
+    void makeNewOrder(std::string username, std::string options, Date date, TaskHandler &taskHandler);
 };
 
 #endif // ORDER_HANDLER_HPP
